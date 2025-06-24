a539cc4e1cdd5ebc35b8ab68bf9c83a099920d39
LEE <lsslcs01@gmail.com>
2025-06-24 15:31:26 +0900

    commit 20250624 LEE

--- src/chapter02/HelloJava.java
+++ src/chapter02/HelloJava.java
@@ -1,18 +1,7 @@
 package chapter02;
 
 public class HelloJava {
-	
-	//한줄 주석
-	
-	/*
-	 * 여러줄 주석
-	 */
-	
-	/**
-	 * 문서형식 주석
-	 * @param args
-	 */
-	
+
 	//main 메소드(실행을 위한 필수 메소드)
 	public static void main(String[] args) {
 		System.out.println("Welcome to JavaWorld!!");
