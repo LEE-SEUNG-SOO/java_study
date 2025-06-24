1dd335d6da25a8bc71223e7a8d6e8572ac40c24a
LEE <lsslcs01@gmail.com>
2025-06-24 17:36:17 +0900

    commit 20250624 LEE

--- src/chapter02/Hello.java
+++ src/chapter02/Hello.java
@@ -3,8 +3,10 @@ package chapter02;
 public class Hello {
 	//main method
 	public static void main(String[] args) {
+		//Welcome to JAVA!!가 출력되는 실행문
 		System.out.println("Welcome to JAVA!!");
+		//Hello가 출력되는 실행문
 		System.out.println("Hello");
-		
 	}
+	
 }
