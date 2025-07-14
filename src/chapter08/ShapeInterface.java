fee3ec23565bb2a70b329e7abb9991c9fa61bd59
LEE <lsslcs01@gmail.com>
2025-07-14 17:11:29 +0900

    commit 20250714 LEE

--- src/chapter08/ShapeInterface.java
+++ src/chapter08/ShapeInterface.java
@@ -1,9 +1,12 @@
 package chapter08;
 /**
  *  추상 메소드들을 모아 놓은 그룹
+ *  Method 에어리어에 생성
  */
 public interface ShapeInterface {
 	public abstract void draw();
 	// 추상 메소드들만 모아두는 곳이기때문에 abstract 키워드를 쓰지 않아도 된다.
 	public double getArea(); 
+	// 상수를 정의해서 사용할 수 있다.(변수 X)
+	public static final String TEST = "test";
 }
