c2c396a9c21b7fd08a1aaeb7294ff8d527d5a7e7
LEE <lsslcs01@gmail.com>
2025-08-04 18:19:28 +0900

    commit 20250804 LEE

--- src/chapter21_mini_project/service/BMPServiceImpl.java
+++ src/chapter21_mini_project/service/BMPServiceImpl.java
@@ -67,8 +67,10 @@ public class BMPServiceImpl implements BMPService {
 	public boolean login(String name, String phone) {
 		boolean result = false;
 		customerRepository = new BMPCustomerDAO();
+		// 이름과 연락처로 고객 정보 취득
 		customer = customerRepository.find(name, phone);
-			
+		
+		// 고객 정보가 존재 할 경우
 		if(customer.getcId() != null) {
 			result = true;
 		}
@@ -90,6 +92,7 @@ public class BMPServiceImpl implements BMPService {
 		// 장바구니에 고객의 데이터가 존재 할 경우
 		if(cartRepository.getCount(customer.getcId()) != 0 ) {
 			List<BMPCartVO> list = new ArrayList<BMPCartVO>();
+			// 장바구니 테이블에서 고객ID기준으로 데이터 취득
 			list = cartRepository.findAll(customer.getcId());
 			
 			// 장바구니 목록 표시
@@ -102,23 +105,27 @@ public class BMPServiceImpl implements BMPService {
 	@Override
 	// 장바구니 비우기
 	public void menuCartClear() {
-		// 장바구니에 고객의 데이터가 존재 할 경우
+		// 장바구니 테이블에 고객의 데이터가 존재 할 경우
 		if(cartRepository.getCount(customer.getcId()) != 0 ) {
 			System.out.print("장바구니의 모든 항목을 삭제하시겠습니까? (Y / N)");
 			String check = scan.next();
 			
 			// y 또는 Y를 입력했을 경우
 			if(check.equals("Y") || check.equals("y")) {
-				// 삭제처리
+				// 장바구니 테이블 삭제처리
 				if(cartRepository.remove(customer.getcId()) != 0) {
+					// 성공
 					System.out.println("장바구니의 모든 항목을 삭제했습니다.");
 				} else {
+					// 실패
 					System.out.println("장바구니 비우기에 실패하였습니다.");
 				}
 			} else {
+				// y 또는 Y 이외의 값을 입력했을경우
 				System.out.println("장바구니 비우기 처리를 취소합니다.");
 			}
 		} else {
+			// 장바구니 테이블에 고객ID의 레코드가 존재 하지 않을경우
 			System.out.println("장바구니가 비어있습니다.");
 		}
 	}
@@ -127,16 +134,19 @@ public class BMPServiceImpl implements BMPService {
 	// 장바구니에 항목 추가하기
 	public void menuCartAddItem() {
 		List<BMPBooksVO> list = new ArrayList<BMPBooksVO>();
+		// 고객 ID설정
 		String cid = customer.getcId();
 		String bid;
 		int result = 0;
 		boolean flag = true;
 		
+		// 도서 테이블의 정보 취득
 		list = booksRepository.findAll();
 		
 		System.out.println("도서 상품 목록 : ");
 		System.out.println("---------------------------------");
 		
+		// 도서 테이블 정보 표시
 		for(BMPBooksVO bmpBooks : list) {
 			System.out.print(bmpBooks.getbId() + " | ");
 			System.out.print(bmpBooks.getbTitle() + " | ");
@@ -153,10 +163,12 @@ public class BMPServiceImpl implements BMPService {
 		
 		// 입력된 도서 ID 확인
 		for(BMPBooksVO bmpBooks : list) {
+			// 입력된 도서 ID가 도서 테이블에 존재 할 경우
 			if(bmpBooks.getbId().equals(bid)) {
+				// 장바구니 테이블에서 고객ID와 도서 ID를 기준으로 레코드 취득
 				BMPCartVO bmpCart = cartRepository.find(cid, bid);
 
-				// 기존에 카트에 존재할 경우
+				// 기존 레코드가 존재할 경우
 				if(bmpCart.getbId() != null) {
 					// 카운트 갯수 1상승
 					bmpCart.setCount(bmpCart.getCount() + 1);
@@ -165,6 +177,7 @@ public class BMPServiceImpl implements BMPService {
 					// 기존 레코드 변경
 					result = cartRepository.update(bmpCart);
 				} else {
+					// 기존 레코드가 존재하지 않을 경우
 					// 도서 아이디 설정
 					bmpCart.setbId(bid);
 					// 카운트 1설정
@@ -174,7 +187,7 @@ public class BMPServiceImpl implements BMPService {
 					// 고객 아이디 설정
 					bmpCart.setcId(cid);
 					
-					// 신규 레코드 추가
+					// 장바구니 테이블에 신규 레코드 추가
 					result = cartRepository.insert(bmpCart);
 				}
 				
@@ -205,6 +218,7 @@ public class BMPServiceImpl implements BMPService {
 			int totalCount = 0;
 			
 			List<BMPCartVO> list = new ArrayList<BMPCartVO>();
+			// 장바구니 테이블에서 고객 ID기준으로 레코드 취득
 			list = cartRepository.findAll(cid);
 			// 장바구니 목록 표시
 			showCartItemList(list);
@@ -212,12 +226,15 @@ public class BMPServiceImpl implements BMPService {
 			System.out.print("장바구니에서 갯수를 줄일 도서의 ID를 입력해주세요. : ");
 			String bid = scan.next();
 			
+			// 취득한 레코드 수만큼 반복
 			for(BMPCartVO bmpCart : list) {
 				// 장바구니에 입력한 도서 아이디가 존재할 경우
 				if(bid.equals(bmpCart.getbId())) {
 					boolean flag = true;
+					// 취득한 레코드의 Count정보
 					int bCount = bmpCart.getCount();
 					
+					// 도서 정보가 수정될때까지 반복
 					while(flag) {
 						totalCount++;
 						// 5회이상 잘못 입력시 초기화면으로 이동
@@ -229,11 +246,12 @@ public class BMPServiceImpl implements BMPService {
 						System.out.print("줄일 갯수를 입력해주세요.(해당 도서의 갯수 "+ bCount + " ) : ");
 						int count = 0; 
 						
+						// 숫자를 입력 했을 경우
 						if(scan.hasNextInt()) {
 							count = scan.nextInt();
-							// 0을 입력한 경우
-							if(count == 0) {
-								System.out.println("0을 입력하셨습니다. 다시 입력해주세요.");
+							// 0이하의 값을 입력한 경우
+							if(count <= 0) {
+								System.out.println("0이하를 입력하셨습니다. 다시 입력해주세요.");
 							}
 							// 장바구니의 갯수가 입력한 값보다 크거나 같을 경우
 							else if(bCount >= count) {
@@ -244,18 +262,21 @@ public class BMPServiceImpl implements BMPService {
 								// 도서 1개 금액 * 도서 카운트
 								bmpCart.setTotal(price * bmpCart.getCount());
 								
+								// 수정된 카운트의 갯수가 0일 경우
 								if(bmpCart.getCount() == 0) {
 									// 도서 카트 테이블에서 삭제
 									cartRepository.remove(cid, bid);
 								} else {
-									// 도서 카트 테이블 갱신
+									// 장바구니 테이블 갱신
 									cartRepository.update(bmpCart);
 								}
+								// 도서 정보 수정 반복 중지
 								flag = false;
 							} else {
 								System.out.println("장바구니에 담겨있는 갯수보다 많이 입력했습니다. 다시 입력해주세요.");
 							}
 						} else {
+							// 숫자외에 다른 문자를 입력했을 경우
 							System.out.println("숫자를 입력해주세요.");
 							scan.next();
 						}
@@ -273,6 +294,7 @@ public class BMPServiceImpl implements BMPService {
 		// 장바구니에 고객의 데이터가 존재 할 경우
 		if(cartRepository.getCount(customer.getcId()) != 0 ) {
 			List<BMPCartVO> list = new ArrayList<BMPCartVO>();
+			// 고객 ID 설정
 			String cid = customer.getcId();
 			// 장바구니 목록 취득
 			list = cartRepository.findAll(cid);
@@ -287,30 +309,37 @@ public class BMPServiceImpl implements BMPService {
 			System.out.print("장바구니에서 삭제할 도서의 ID를 입력해주세요. : ");
 			bid = scan.next();
 			
+			// 도서 카트의 리스트만큼 반복
 			for(BMPCartVO bmpCart : list) {
 				// 장바구니에 입력한 도서 아이디가 존재할 경우
 				if(bid.equals(bmpCart.getbId())) {
 					System.out.print("장바구니의 항목을 삭제 하시겠습니까? (Y / N)");
 					String check = scan.next();
 					
+					// Y 또는 y를 입력했을 경우
 					if(check.equals("Y") || check.equals("y")) {
+						// 장바구니 테이블에서 해당 도서정보 삭제
 						int result = cartRepository.remove(cid, bid);
 						
+						// 장바구니 테이블의 레코드 삭제 성공
 						if(result != 0) {
 							System.out.println(bmpCart.getbId() + " 도서가 장바구니에서 삭제되었습니다.");
 						} else {
+							// 장바구니 테이블의 레코드 삭제 실패
 							System.out.println(bmpCart.getbId() + " 도서의 장바구니 삭제에 실패하였습니다.");
 						}
 						
 					} else {
+						// Y 또는 y 이외의 값을 입력했을 경우
 						System.out.println("장바구니 삭제 처리를 취소합니다.");
 					}
-					
+					// 장바구니에 도서ID 존재 유무 설정
 					flag = false;
 					break;
 				}			
 			}
 			
+			// 입력한 도서ID 가 존재 하지 않았을 경우
 			if(flag) {
 				System.out.println("존재하지 않는 도서 번호입니다.");
 			}
@@ -320,6 +349,7 @@ public class BMPServiceImpl implements BMPService {
 	}
 
 	@Override
+	// 영수증 출력
 	public void menuCartBill() {
 		// 장바구니에 고객의 데이터가 존재 할 경우
 		if(cartRepository.getCount(customer.getcId()) != 0 ) {
@@ -336,12 +366,13 @@ public class BMPServiceImpl implements BMPService {
 	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 	        String formattedDate = now.format(formatter);
 			
-	        // Y 또는 y를 입력할 경우 기존 고객 정보 입력
+	        // Y 또는 y를 입력했을 경우 기존 고객 정보 입력
 			if(check.equals("Y") || check.equals("y")) {
 				name = customer.getcName();
 				phone = customer.getcPhone();
 				addr = customer.getcAddr();
 			} else {
+				// Y 또는 y 이외의 값을 입력했을 경우
 				System.out.print("배송받을 고객명을 입력해주세요. : ");
 				name = scan.next();
 				
@@ -360,6 +391,7 @@ public class BMPServiceImpl implements BMPService {
 			
 			// 장바구니 리스트
 			List<BMPCartVO> list = new ArrayList<BMPCartVO>();
+			// 장바구니 테이블에 고객ID기준으로 레코드 취득
 			list = cartRepository.findAll(customer.getcId());
 			
 			// 장바구니 목록 표시
@@ -383,6 +415,8 @@ public class BMPServiceImpl implements BMPService {
 		System.out.println("---------------------------------");
 		System.out.println("도서ID\t\t 수량\t 합계");
 		
+		// 장바구니 테이블의 레코드 출력
+		// 도서ID, 갯수, 총금액
 		for(BMPCartVO bmpCart : list) {
 			System.out.println(bmpCart.getbId()+ "\t " + bmpCart.getCount() + "\t " + bmpCart.getTotal());
 		}
